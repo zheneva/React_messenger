02a5b17d0623ecf4e4061ec7c91fc33dcc5cf52a
zheneva <mr_zhenkins@mail.ru>
2022-08-08 07:29:28 +0300

    feat: add header index.js/index.css

--- react-dialog-example/src/App.js
+++ react-dialog-example/src/App.js
@@ -1,9 +1,10 @@
-import React from "react"
+import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div className="container">
-      <h1>Hello world</h1>
+      <Header />
     </div>
   );
 };
---
+++ react-dialog-example/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import UserInfo from "./UserInfo";
+
+import "./styles.css";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <UserInfo />
+        </div>
+    );
+};
+
+export default Header;
